80478c18e8f30cbcf90aa16ed1b858545c45ac61
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-11 22:18:20 +0200

    TOUT EST OK POUR COMMENCER À UTILISER L'API

--- Docker/server/server.js
+++ Docker/server/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const app = express();
-
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const express = require('express')
+const app = express()
+const port = process.env.HOST_MACHINE_HTTP_PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World!')
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
