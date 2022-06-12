75104d896599c5cdab0081362689fa6ae7a77e82
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-12 22:13:54 +0200

    OK pour utilisation de ce token

--- Docker/server/getMe.js
+++ Docker/server/getMe.js
@@ -1,13 +1,12 @@
 const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node')
-// const token = JSON.parse(fs.readFileSync('token.json'))
-const token = 'BQBXNhSRZRF-p_giiv-DC9nvbeM-VBvPjbvANfotsIkXtesPnbX24lkypzEsQSHoAEQ2VF-HXToV2CajISxkJOAi1fEILaHl3GExJGPEq_XhZXWw_H6hyt8nuvTyYDNRGP8whxl_jlsrLV_8aQaExRd5EEm7NvbEIy8_frpITS8uA6slhLmthEW7ymkVTM9zSM0MmKxJrRUybouFOp92FL1z6ohnL6-cfaUiq3-o6YcZSd29hx6DwnKu1Tr4DGjnuP7l9AKK1XqEm70OYJtZwDd3D2hPHmzyf0dGzVL-Tbqys47KcUwsEYnMC5AczKAP8nDTzpug3UQVsn6HYagrKA'
-
+const token = fs.readFileSync('token.json', 'utf8')
 const spotifyApi = new SpotifyWebApi()
 spotifyApi.setAccessToken(token)
 
 async function getDataAboutMe(){
     let me = await spotifyApi.getMe()
+    console.log(me)
     return me
     // getMyPlaylists(me.body.id)
 }
@@ -17,8 +16,8 @@ async function getMyPlaylists(idUser){
     console.log(data.body.items)
 }
 
-
-
 module.exports = {
     getDataAboutMe : getDataAboutMe,
 }
+
+getDataAboutMe()
