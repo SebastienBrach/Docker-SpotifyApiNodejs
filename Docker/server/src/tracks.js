6d471c1e017604b7822d2d64ef9c9c15d48254a4
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-15 21:51:34 +0200

    Update tracks.js

--- Docker/server/src/tracks.js
+++ Docker/server/src/tracks.js
@@ -5,15 +5,26 @@ const spotifyApi = new SpotifyWebApi()
 spotifyApi.setAccessToken(token)
 
 
+async function getAlbumTracks(albumID, limit, offset){
+    let tracks = await spotifyApi.getAlbumTracks(albumID, {limit:limit, offset :offset})
+    return tracks.body
+}
+
+async function getArtistTopTracks(artistID, location){
+    let tracks = await spotifyApi.getArtistTopTracks(artistID, location)
+    return tracks.body
+}
+
 // spotifyApi.searchTracks('Love') => tracks whose name, album or artist contains 'Love'
 // spotifyApi.searchTracks('artist:Love') => tracks whose artist's name contains 'Love'
 // spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar') => tracks whose artist's name contains 'Kendrick Lamar', and track name contains 'Alright'
-
-async function searchTracks(){
-    let track = await spotifyApi.searchTracks()
+async function searchTracks(data){
+    let track = await spotifyApi.searchTracks(data)
     return track.body
 }
 
 module.exports = {
+    getAlbumTracks: getAlbumTracks,
+    getArtistTopTracks : getArtistTopTracks,
     searchTracks : searchTracks,
 }
