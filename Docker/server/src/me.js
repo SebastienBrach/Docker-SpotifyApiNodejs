545a6472c547a106f50a7ee196bcb5d98c89f130
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-15 22:11:42 +0200

    Update me.js

--- Docker/server/src/me.js
+++ Docker/server/src/me.js
@@ -1,67 +1,3 @@
-// const fs = require('fs')
-// const SpotifyWebApi = require('spotify-web-api-node')
-// const token = fs.existsSync(process.env.TOKEN_FILE) ? fs.readFileSync(process.env.TOKEN_FILE, 'utf8') : ""
-// const spotifyApi = new SpotifyWebApi()
-// spotifyApi.setAccessToken(token)
-
-// async function getDataAboutMe(){
-//     let me = await spotifyApi.getMe()
-//     return me
-// }
-
-// async function getMyPlaylists(idUser){
-//     const data = await spotifyApi.getUserPlaylists(idUser)
-//     return data.body.items
-// }
-
-// async function getFollowedArtists(){
-//     const data = await spotifyApi.getUserPlaylists({ limit : 50 })
-//     return data.body.artists
-// }
-
-// async function getMySavedTracks(){
-//     const data = await spotifyApi.getMySavedTracks({
-//         limit : 50,
-//         offset: 1
-//       })
-//     return data.body.items
-// }
-
-// async function getMySavedAlbums(){
-//     const data = await spotifyApi.getMySavedTracks({
-//         limit : 2,
-//         offset: 1
-//       })
-//     return data.body.items
-// }
-
-// async function getMyTopArtists(){
-//     const data = await spotifyApi.getMyTopArtists()
-//     return data.body.items
-// }
-
-// async function getMyTopTracks(){
-//     const data = await spotifyApi.getMyTopTracks({ limit : 50 })
-//     return data.body.items
-// }
-
-// module.exports = {
-//     getDataAboutMe : getDataAboutMe,
-//     getMyPlaylists : getMyPlaylists,
-//     getFollowedArtists : getFollowedArtists,
-//     getMySavedTracks : getMySavedTracks,
-//     getMySavedAlbums : getMySavedAlbums,
-//     getMyTopArtists : getMyTopArtists,
-//     getMyTopTracks : getMyTopTracks,
-// }
-
-
-
-
-
-
-
-
 const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node')
 
@@ -93,7 +29,9 @@ class Me {
     }
     
     async getFollowedArtists(){
-        const data = await this.spotifyApi.getUserPlaylists({ limit : 50 })
+        const data = await this.spotifyApi.getUserPlaylists({ 
+            limit : 50 
+        })
         return data.body.artists
     }
     
