8f33dd92669579c39f4c4888012671e69cd986aa
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-15 22:11:08 +0200

    OK

--- Docker/server/src/me.js
+++ Docker/server/src/me.js
@@ -1,56 +1,131 @@
+// const fs = require('fs')
+// const SpotifyWebApi = require('spotify-web-api-node')
+// const token = fs.existsSync(process.env.TOKEN_FILE) ? fs.readFileSync(process.env.TOKEN_FILE, 'utf8') : ""
+// const spotifyApi = new SpotifyWebApi()
+// spotifyApi.setAccessToken(token)
+
+// async function getDataAboutMe(){
+//     let me = await spotifyApi.getMe()
+//     return me
+// }
+
+// async function getMyPlaylists(idUser){
+//     const data = await spotifyApi.getUserPlaylists(idUser)
+//     return data.body.items
+// }
+
+// async function getFollowedArtists(){
+//     const data = await spotifyApi.getUserPlaylists({ limit : 50 })
+//     return data.body.artists
+// }
+
+// async function getMySavedTracks(){
+//     const data = await spotifyApi.getMySavedTracks({
+//         limit : 50,
+//         offset: 1
+//       })
+//     return data.body.items
+// }
+
+// async function getMySavedAlbums(){
+//     const data = await spotifyApi.getMySavedTracks({
+//         limit : 2,
+//         offset: 1
+//       })
+//     return data.body.items
+// }
+
+// async function getMyTopArtists(){
+//     const data = await spotifyApi.getMyTopArtists()
+//     return data.body.items
+// }
+
+// async function getMyTopTracks(){
+//     const data = await spotifyApi.getMyTopTracks({ limit : 50 })
+//     return data.body.items
+// }
+
+// module.exports = {
+//     getDataAboutMe : getDataAboutMe,
+//     getMyPlaylists : getMyPlaylists,
+//     getFollowedArtists : getFollowedArtists,
+//     getMySavedTracks : getMySavedTracks,
+//     getMySavedAlbums : getMySavedAlbums,
+//     getMyTopArtists : getMyTopArtists,
+//     getMyTopTracks : getMyTopTracks,
+// }
+
+
+
+
+
+
+
+
 const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node')
-const token = fs.existsSync(process.env.TOKEN_FILE) ? fs.readFileSync(process.env.TOKEN_FILE, 'utf8') : ""
-const spotifyApi = new SpotifyWebApi()
-spotifyApi.setAccessToken(token)
 
-async function getDataAboutMe(){
-    let me = await spotifyApi.getMe()
-    return me
-}
+class Me {
 
-async function getMyPlaylists(idUser){
-    const data = await spotifyApi.getUserPlaylists(idUser)
-    return data.body.items
-}
+    constructor(){
+        this.token = fs.existsSync(process.env.TOKEN_FILE) ? fs.readFileSync(process.env.TOKEN_FILE, 'utf8') : ""
+        this.spotifyApi = new SpotifyWebApi()
+        this.spotifyApi.setAccessToken(this.token)
+    }
 
-async function getFollowedArtists(){
-    const data = await spotifyApi.getUserPlaylists({ limit : 50 })
-    return data.body.artists
-}
+    async getAllMyAccountData(){
+        this.getMe = await this.getDataAboutMe()
+        this.getMyPlaylists = await this.getMyPlaylists(this.getMe.id)
+        this.getFollowedArtists = await this.getFollowedArtists()
+        this.getMySavedTracks = await this.getMySavedTracks()
+        this.getMySavedAlbums = await this.getMySavedAlbums()
+        this.getMyTopArtists = await this.getMyTopArtists()
+        this.getMyTopTracks = await this.getMyTopTracks()
+    }
 
-async function getMySavedTracks(){
-    const data = await spotifyApi.getMySavedTracks({
-        limit : 50,
-        offset: 1
-      })
-    return data.body.items
-}
+    async getDataAboutMe(){
+        return await this.spotifyApi.getMe()
+    }
+    
+    async getMyPlaylists(idUser){
+        const data = await this.spotifyApi.getUserPlaylists(idUser)
+        return data.body.items
+    }
+    
+    async getFollowedArtists(){
+        const data = await this.spotifyApi.getUserPlaylists({ limit : 50 })
+        return data.body.artists
+    }
+    
+    async getMySavedTracks(){
+        const data = await this.spotifyApi.getMySavedTracks({
+            limit : 50,
+            offset: 1
+          })
+        return data.body.items
+    }
+    
+    async getMySavedAlbums(){
+        const data = await this.spotifyApi.getMySavedTracks({
+            limit : 2,
+            offset: 1
+          })
+        return data.body.items
+    }
+    
+    async getMyTopArtists(){
+        const data = await this.spotifyApi.getMyTopArtists()
+        return data.body.items
+    }
+    
+    async getMyTopTracks(){
+        const data = await this.spotifyApi.getMyTopTracks({ limit : 50 })
+        return data.body.items
+    }
 
-async function getMySavedAlbums(){
-    const data = await spotifyApi.getMySavedTracks({
-        limit : 2,
-        offset: 1
-      })
-    return data.body.items
-}
 
-async function getMyTopArtists(){
-    const data = await spotifyApi.getMyTopArtists()
-    return data.body.items
-}
-
-async function getMyTopTracks(){
-    const data = await spotifyApi.getMyTopTracks({ limit : 50 })
-    return data.body.items
 }
 
 module.exports = {
-    getDataAboutMe : getDataAboutMe,
-    getMyPlaylists : getMyPlaylists,
-    getFollowedArtists : getFollowedArtists,
-    getMySavedTracks : getMySavedTracks,
-    getMySavedAlbums : getMySavedAlbums,
-    getMyTopArtists : getMyTopArtists,
-    getMyTopTracks : getMyTopTracks,
+    instance : Me,
 }
--- Docker/server/src/server.js
+++ Docker/server/src/server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config();
 const port = 3000
-const me = require("./me.js");
+const Me = require("./me.js");
 const Spotify = require("./spotify.js");
 
-let spotify = new Spotify.instance()
+const spotify = new Spotify.instance()
+const myData = new Me.instance()
 
 app.get('/', (req, res) => {
   res.redirect(spotify.redirectToCallback())
@@ -13,27 +14,11 @@ app.get('/', (req, res) => {
 
 app.get('/auth', async (req, res) => {
   let test = await spotify.auth(req)
-  res.redirect('/getMe')
+  res.redirect('/getAllMyAccountData')
 })
 
-app.get('/getMe', async (req, res) => {
-  const myAccountData = await me.getDataAboutMe()
-  const myPlaylists = await me.getMyPlaylists(myAccountData.body.id)
-  const myFollowedArtists = await me.getFollowedArtists()
-  const mySavedTracks = await me.getMySavedTracks()
-  const mySavedAlbums = await me.getMySavedAlbums()
-  const myTopArtists = await me.getMyTopArtists()
-  const myTopTracks = await me.getMyTopTracks()
-  const myData = {
-    myAccountData: myAccountData,
-    myPlaylists: myPlaylists,
-    myFollowedArtists: myFollowedArtists,
-    mySavedTracks: mySavedTracks,
-    mySavedAlbums: mySavedAlbums,
-    myTopArtists: myTopArtists,
-    myTopTracks: myTopTracks,
-  }
-  console.log(process.env)
+app.get('/getAllMyAccountData', async (req, res) => {
+  await myData.getAllMyAccountData()
   res.json(myData)
 })
 
