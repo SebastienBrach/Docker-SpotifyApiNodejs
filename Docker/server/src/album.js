d52e82e57604c6ce726c0e15a8f08d8ac499bb03
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-17 23:26:11 +0200

    OK

--- Docker/server/src/album.js
+++ Docker/server/src/album.js
@@ -2,4 +2,5 @@
 // ALBUM_GET=/album/get
 // ALBUM_GET_TRACKS=/album/getTracks
 // ALBUM_ADD_TO_SAVED_ALBUMS=/album/addToSavedAlbums
+// ALBUM_REMOVE_FROM_SAVED_ALBUMS=/album/removeFromSavedAlbums
 // ###
