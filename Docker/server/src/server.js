07e451eec056e11e470b2c296298dd7a2030054b
SebastienBrach <sebastienbrach11@gmail.com>
2022-06-20 21:42:06 +0200

    VOIR POUR LA GESTION DES ERREURS

--- Docker/server/src/server.js
+++ Docker/server/src/server.js
@@ -6,12 +6,18 @@ const log = require("./Config/log.js");
 const Spotify = require("./Auth/SpotifyAPIAuth.js");
 const spotify = new Spotify.instance()
 
+
+const meRoute = require("./Routes/Me.js");
+app.use(process.env.ME_BASE_ROUTE, meRoute);
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
   log.info("SERVER", `METHOD ${req.method} - URL ${req.url} - IP ${req.socket.remoteAddress}`);
+
   res.on('finish', () => {
     log.info("SERVER", `METHOD ${req.method} - URL ${req.url} - IP ${req.socket.remoteAddress} - STATUS ${res.statusCode}`);
   });
@@ -23,18 +29,16 @@ app.use((req, res, next) => {
 
   if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', 'GET PATC DELETE POST PUT');
-      return res.status(200).json({});
+      return res.status(200).send({});
   }
 });
 
 app.use((req, res, next) => {
   const error = new Error('Not Found');
-  return res.status(404).json({ error: error.message });
+  console.log(error)
+  return res.status(404).send({ error: error.message });
 });
 
-
-const meRoute = require("./Routes/Me.js");
-app.use(process.env.ME_BASE_ROUTE, meRoute);
 
 app.get('/', (req, res) => {
   res.redirect(spotify.redirectToCallback())
